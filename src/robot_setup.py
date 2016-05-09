319b3ce46d3deeb5d11068f22e2463f05e3a098d
Kevin Chou <k29645806@gmail.com>
2016-05-09 16:53:18 +0800

    workable version robot.launch

--- src/robot_setup.py
+++ src/robot_setup.py
@@ -28,24 +28,24 @@ class Robot(object):
                          rospy.Time.now(),
                          "odom",
                          "map")
-        self.tfBroadcaster.sendTransform((0, 0, 0),
+        self.tfBroadcaster.sendTransform((0, 0, 0.3),
                          quaternion_from_euler(0, 0, 0),
                          rospy.Time.now(),
                          "base_link",
                          "base_footprint")
-        self.tfBroadcaster.sendTransform((0.0, 0.0, 0.0),
+        self.tfBroadcaster.sendTransform((0.3, 0.0, 0.0),
                          quaternion_from_euler(0, 0, 0),
                          rospy.Time.now(),
                          "camera_link",
                          "base_link")
-        self.tfBroadcaster.sendTransform((0.0, 0.0, 0.0),
+        self.tfBroadcaster.sendTransform((0.3, 0.0, 0.0),
                          quaternion_from_euler(0, 0, 0),
                          rospy.Time.now(),
                          "base_laser",
                          "base_link")
 
     def start(self):
-        rate = rospy.Rate(20.0)
+        rate = rospy.Rate(50.0)
         rospy.loginfo("Robot Engine Start")
         while not rospy.is_shutdown():
             self.broadcastTF()
